5eb78bc05dfc0c5eb0a7aa83c6ff991c9123ae2b
Andrey Knupp Vital <andreykvital@gmail.com>
2011-10-11 21:10:03 -0300

    PHP Permutations

---
+++ Permutations/Example.php
@@ -0,0 +1,4 @@
+<?php
+	require_once 'Permutations.php' ;
+	$Permutations = getCombinations( Array( 1 , 2 , 3 ) ) ;
+	print_r( $Permutations ) ;
---
+++ Permutations/Permutations.php
@@ -0,0 +1,30 @@
+<?php 
+	 /**
+	  * Recupera todas permutações / combinações de uma matriz
+	  * O limite de índices / valores no array deve ser de cinco
+	  * Pois mais que isso, é provável que entre tempo de execução infinito
+	  * @param Array $Array 
+	  * @param Array $Permutations
+	  * @throws OutOfRangeException se o número de índices / valores no array for maior que 5
+	  */
+	 function getCombinations ( Array $Array , Array $Permutations = Array ( ) ) {
+		   static $permutedItems ;
+		   $FlattenArray = Array( );
+		   forEach( new RecursiveIteratorIterator ( new RecursiveArrayIterator ( $Array ) ) as $Data ) 
+			     $FlattenArray [ ] = $Data ; 
+			     if( count( $FlattenArray ) > 5 ) 
+				  throw new OutOfRangeException( 'Não podemos gerar permutações com mais de 5 matrizes' );
+		   $Array = array_filter ( array_unique ( $FlattenArray ) ) ;
+		   if( count ( $Array ) ) {
+			 for ( $i = ( count ( $Array ) - 1 ) ; $i >= 0 ; -- $i ) :
+				 $newArray = $Array ;
+				 $newPermutations = $Permutations ;
+				 list ( $item ) = array_splice ( $newArray , $i , 1 ) ; 
+				 array_unshift ( $newPermutations , $item ) ;
+				 getCombinations ( $newArray , $newPermutations ) ;
+			 endfor; 
+			 return $permutedItems;
+		   } else $permutedItems [ ] = $Permutations;
+		   
+	 }
+	 
