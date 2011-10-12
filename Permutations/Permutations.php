b5fc7e89c0351337ccfa01612213560c86091e37
Andrey Knupp Vital <andreykvital@gmail.com>
2011-10-11 21:35:12 -0300

    PHP Permutations, Licensed With GPL v3.0

--- Permutations/Permutations.php
+++ Permutations/Permutations.php
@@ -3,8 +3,24 @@
 	  * Recupera todas permutações / combinações de uma matriz
 	  * O limite de índices / valores no array deve ser de cinco
 	  * Pois mais que isso, é provável que entre tempo de execução infinito
-	  * @param Array $Array 
-	  * @param Array $Permutations
+	  * 
+	  * Copyright (C) <2011-2012>  <Andrey Knupp Vital>
+	  *
+	  * This program is free software: you can redistribute it and/or modify
+	  * it under the terms of the GNU General Public License as published by
+	  * the Free Software Foundation, either version 3 of the License, or
+	  * (at your option) any later version.
+	  *
+	  * This program is distributed in the hope that it will be useful,
+	  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	  * GNU General Public License for more details.
+	  *
+ 	  * You should have received a copy of the GNU General Public License
+ 	  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+         * @author Andrey Knupp Vital
+	  * @param  Array $Array 
+	  * @param  Array $Permutations
 	  * @throws OutOfRangeException se o número de índices / valores no array for maior que 5
 	  */
 	 function getCombinations ( Array $Array , Array $Permutations = Array ( ) ) {
